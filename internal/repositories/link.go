799a446ff7ccc55d2221636a360fe3319627c404
htungg <lehoangtung081003@gmail.com>
2024-03-03 18:05:18 +0700

    feat: finish query wordpress db function

--- internal/repositories/link.go
+++ internal/repositories/link.go
@@ -22,7 +22,7 @@ func (_this *linkRepo) GetPostsByTitle(db *db.DB, name string) ([]Post, error) {
 	var posts []Post
 	processed := strings.TrimSpace(name[7:])
 	err := db.DB().Table("wplw_posts").
-		Select("post_title AS title", "guid AS link").
+		Select("post_title AS title, guid AS link").
 		Where("post_title LIKE ?", "%"+processed+"%").
 		Where("post_status = ?", "publish").
 		Limit(5).
