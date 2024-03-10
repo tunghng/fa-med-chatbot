fd67534d1226aa1ed4ae76f028b487c0e98a7ef1
htungg <lehoangtung081003@gmail.com>
2024-03-10 15:59:43 +0700

    feat: add init tracking user database

--- cmd/medi-query-bot/internal/providers/init.go
+++ cmd/medi-query-bot/internal/providers/init.go
@@ -61,6 +61,25 @@ func newMySQLConnection() *db.DB {
 	return _db
 }
 
+func newMySQLUserTrackingConnection() *db.DB {
+	_db, err := db.Connect(&db.Config{
+		//Driver:   db.DriverMySQL,
+		//Username: viper.GetString(cfg.ConfigKeyDBMySQLUsername),
+		//Password: viper.GetString(cfg.ConfigKeyDBMySQLPassword),
+		//Host:     viper.GetString(cfg.ConfigKeyDBMySQLHost),
+		//Port:     viper.GetInt64(cfg.ConfigKeyDBMySQLPort),
+		Username: "root",
+		Password: "tungoccho123",
+		Host:     "localhost",
+		Port:     3306,
+		Database: "tracking_test",
+	})
+	if err != nil {
+		log.Fatalf("Connecting to MySQL DB: %v", err)
+	}
+	return _db
+}
+
 func newCfgReader() *viper.Viper {
 	v := viper.New()
 	v.AutomaticEnv()
